27eb33080a4aaf525bcff5a8840d49b59ad6432c
MD. Maynul Hossain <maynulhossain69@gmail.com>
2025-07-26 23:46:26 +0600

    homepage-till-guest

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Hero from '../Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonial from '../components/Testimonial'
 
 
 const Home = () => {
   return (
       <>
-          <Hero />
+      <Hero />
+      <FeaturedDestination />
+      <ExclusiveOffer />
+      <Testimonial />
       </>
   )
 }
