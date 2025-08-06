774487ddda8a903754d7544ee0519972329958c4
MD. Maynul Hossain <maynulhossain69@gmail.com>
2025-08-07 02:05:47 +0600

    front-end design-complete!!!!!!

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-      <div className='flex flex-col h-screen'>
+      <div className='flex flex-col '>
           <Navbar />
           <div className='flex h-full'>
               <SidebBar />
